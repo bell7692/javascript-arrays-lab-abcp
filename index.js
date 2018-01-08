e479f3a98cfc0769bb0de3689c4298116643d984
Bo Eun Lee <bolee417@gmail.com>
2018-01-08 19:52:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const app = "I don't do much."
 
+function destructivelyAppendKitten(name) {
+  app.push(name);
+}
+function destructivelyPrependKitten(name) {
+  app.unshift(name);
+}
+function destructivelyRemoveLastKitten() {
+  app.pop();
+}
+function destructivelyRemoveFirstKitten() {
+  app.shift()
+}
+function appendKitten(name) {
+  
+}
+function prependKitten(name) {
+  
+}
+
